5165eb93f89400ae0817bab2d6038240c090c353
Paolo Castagna <paolo.castagna@gmail.com>
2011-12-08 18:22:07 +0000

    Added three JRuby examples on how to use Apache Jena from JRuby.

---
+++ src/main/jruby/hello_jena.rb
@@ -0,0 +1,12 @@
+require 'java'
+
+java_import java.lang.System
+java_import org.apache.jena.examples.Utils
+java_import com.hp.hpl.jena.rdf.model.ModelFactory
+
+input = Utils.getResourceAsStream("data/data.ttl")
+model = ModelFactory.createDefaultModel()
+model.read(input, nil, "TURTLE")
+
+puts "\n---- Turtle ----"
+model.write(System.out, "TURTLE")
