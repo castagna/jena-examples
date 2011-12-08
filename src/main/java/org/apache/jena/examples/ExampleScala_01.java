59a1950d3a31e259338f9c6d82d28e85383675c2
Paolo Castagna <paolo.castagna@gmail.com>
2011-12-08 21:54:11 +0000

    Added a couple of Scala trivial examples.

---
+++ src/main/java/org/apache/jena/examples/ExampleScala_01.java
@@ -0,0 +1,10 @@
+package org.apache.jena.examples;
+
+public class ExampleScala_01 {
+
+    public static void main(String[] args) {
+        // TODO: Install http://www.scala-ide.org/, it requires: Eclipse Classic 3.6.2 or 3.7
+        // HelloJena.main(new String[]{});
+    }
+
+}
---
+++ src/main/scala/hello_jena.scala
@@ -0,0 +1,16 @@
+
+import _root_.java.lang.System
+import _root_.org.apache.jena.examples.Utils
+import _root_.com.hp.hpl.jena.rdf.model.ModelFactory
+
+object HelloJena {
+    def main(args: Array[String]) {
+        val input = Utils.getResourceAsStream("data/data.ttl")
+
+        val model = ModelFactory.createDefaultModel()
+        model.read(input, null, "TURTLE")
+
+        System.out.println("\n---- Turtle ----")
+        model.write(System.out, "TURTLE")
+    }
+}
