948459bdf513a7a24f39bc301bf0e4de4ba14f85
Paolo Castagna <paolo.castagna@gmail.com>
2012-03-21 14:43:51 +0000

    Use StoreConnection not TDBFactory.

--- src/main/java/dev/TestTDBCorruption.java
+++ src/main/java/dev/TestTDBCorruption.java
@@ -1,13 +1,28 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.util.Iterator;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openjena.atlas.lib.FileOps;
 import org.openjena.riot.Lang;
 import org.openjena.riot.RiotLoader;
 
@@ -15,7 +30,7 @@ import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.sparql.core.DatasetGraph;
 import com.hp.hpl.jena.sparql.core.Quad;
-import com.hp.hpl.jena.tdb.TDBFactory;
+import com.hp.hpl.jena.tdb.StoreConnection;
 import com.hp.hpl.jena.tdb.base.file.Location;
 
 public class TestTDBCorruption {
@@ -26,8 +41,17 @@ public class TestTDBCorruption {
    
     @Test public void test() {
         Location location = new Location ( path );
-        Dataset dataset = TDBFactory.createDataset ( location );
-        dataset.begin ( ReadWrite.WRITE );
+        
+        //
+        // This is wrong: if transaction are needed, 
+        // StoreConnection must be used instead of TDBFactory
+        //
+        // Dataset dataset = TDBFactory.createDataset ( location );
+        // dataset.begin ( ReadWrite.WRITE );
+        // 
+        
+        StoreConnection sc = StoreConnection.make ( location );
+        Dataset dataset = sc.begin ( ReadWrite.WRITE ).toDataset();
         try {
             DatasetGraph dsg = dataset.asDatasetGraph();
             DatasetGraph dsg2 = RiotLoader.datasetFromString ( str_triple, Lang.TURTLE, null );
